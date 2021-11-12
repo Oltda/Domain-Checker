64105bafec88dcb8b23b7cf2314130138591039d
Daniel Olt <dan.olt@hotmail.cz>
2021-11-12 22:50:41 +0700

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css';
+import CheckDomain from './components/CheckDomain';
+
+function App() {
+
+
+
+  return (
+    <div className="App">
+        <CheckDomain />
+
+    </div>
+  );
+}
+
+export default App;
